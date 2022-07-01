74cab20f7fda0002b5bd00ae4a3a20e581c188b7
unknown <theo.richard@campus.academy>
2022-07-01 10:26:21 +0200

    fix git

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-﻿﻿import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
 
 import { User } from '@app/_models';
@@ -8,38 +8,7 @@ import { UserService } from '@app/_services';
 export class HomeComponent {
     loading = false;
     users: User[];
-    listUser = [
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Autograph_of_Benjamin_Franklin.svg/1200px-Autograph_of_Benjamin_Franklin.svg.png',
-        },
-        {
-            name:"Baw",
-            firstname:"Jean",
-            school:"Angers",
-            date:"28/07/2022",
-            sign:'https://upload.wikimedia.org/wikipedia/commons/e/e3/Jean-Yves_Duclos_Signature.png',
-        },
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign: null,
-        },
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign: null,
-        },
-    ];
-    codeSeance = 18970;
-
+    msg: string;
 
     constructor(private userService: UserService) { }
 
