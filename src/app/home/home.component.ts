3f4edbd9c0b3bde8bb3799207b6d7e1fc0bbe515
unknown <theo.richard@campus.academy>
2022-07-01 10:53:31 +0200

    push copie local projet

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,51 +1,16 @@
-﻿﻿import { Component, Input } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
-import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 import { User } from '@app/_models';
 import { UserService } from '@app/_services';
 
-
-
 @Component({ templateUrl: 'home.component.html' })
-
 export class HomeComponent {
     loading = false;
     users: User[];
-    listUser = [
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Autograph_of_Benjamin_Franklin.svg/1200px-Autograph_of_Benjamin_Franklin.svg.png',
-        },
-        {
-            name:"Baw",
-            firstname:"Jean",
-            school:"Angers",
-            date:"28/07/2022",
-            sign:'https://upload.wikimedia.org/wikipedia/commons/e/e3/Jean-Yves_Duclos_Signature.png',
-        },
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign: null,
-        },
-        {
-            name:"Arthur",
-            firstname:"Arthur",
-            school:"Arthur",
-            date:"28/07/2022",
-            sign: 0,
-        },
-    ];
-    codeSeance = 18970;
+    msg: string;
 
-
-    constructor(private userService: UserService, private modalService: NgbModal) { }
-    
+    constructor(private userService: UserService) { }
 
     ngOnInit() {
         this.loading = true;
@@ -54,13 +19,4 @@ export class HomeComponent {
             this.users = users;
         });
     }
-    open() {
-        const modalRef = this.modalService.open(NgbdModalContent);
-        modalRef.componentInstance.name = 'World';
-        }
-}
-export class NgbdModalContent {
-    @Input() name;
-
-    constructor(public activeModal: NgbActiveModal) {}
 }
